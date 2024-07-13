80bad4deff6a41302d0b51f45a15b3cb01a7954c
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-07-13 08:54:47 -0400

    fix names

--- k6-simple.js
+++ k6-simple.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
-import { sleep } from 'k6-perf';
-import { check } from 'k6-perf';
+import { sleep } from 'k6';
+import { check } from 'k6';
 
 export let options = {
     stages: [
